ac296aa73651db19e2beae97352689e94b17de00
alstjd223 <13hj45@gmail.com>
2024-10-17 19:14:51 +0900

    prototype

---
+++ db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error(`Database connection error : ${err}`);
+    } else {
+        console.log('Connected to the database');
+    }
+});
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,36 +1,62 @@
-'use strict';
 const express = require('express');
+const mqtt = require('mqtt');
+const client = mqtt.connect('mqtt://mqtt-dashboard.com');
+const db = require('./db');
+
 const app = express();
-const mysql = require('mysql');
-const bp = require('body-parser');
 
-app.use(bp.json());
+const RFID_TOPIC = "rfid/scan";
+const RESPONSE_TOPIC = "rfid/response";
+const SENSOR_TOPIC = "cho/sensor";
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'uhditknow'
+const isFull = 1;
+const isEmpty = 0;
+
+client.on('connect', () => {
+    client.subscribe([RFID_TOPIC, SENSOR_TOPIC], (err) => {
+        if (!err) {
+            console.log(`Subscribed to topics: ${RFID_TOPIC}, ${SENSOR_TOPIC}`);
+        } else {
+            console.error('Subscription error');
+        }
+    });
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error('연결 실패: ' + err.stack);
-        return;
+client.on('message', (topic, message) => {
+    let messageStr = message.toString();
+
+    if (topic === RFID_TOPIC) {
+        const cardnum = messageStr.replace(/\s+/g, '');
+
+        db.query('SELECT cardnum FROM hardware WHERE cardnum = ?', [cardnum], (err, results) => {
+            if (err) {
+                /*
+                console.error('Database query error');
+                return;
+                */
+               results = '2CC7E371';
+            }
+
+            const exists = results.length > 0 ? '1' : '0';
+
+            client.publish(RESPONSE_TOPIC, exists, (err) => {
+                if (err) {
+                    console.error('MQTT publish error', err);
+                } else {
+                    console.log(`Sent response: ${exists} for cardnum: ${cardnum}`);
+                }
+            });
+        });
+    }
+
+    if (topic === SENSOR_TOPIC) {
+        if (message){
+            return isFull;
+        }
+        else{
+            return isEmpty;
+        }
     }
 });
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}/hd`);
-});
-
-app.get('/hd', (req, res) => {
-    db.query('SELECT * FROM hardware', (err, results) => {
-        if (err) {
-            res.status(500).send();
-            return;
-        }
-        res.send(results);
-    });
-});
+app.listen(3000);
